f30c33ecde5480e0a32e83a2472c65fcc2968261
coolepoff <97253599+coolepoff@users.noreply.github.com>
2022-08-10 12:36:48 +0300

    добавляет модуль "фильтры"

--- src/main.js
+++ src/main.js
@@ -1,1 +1,7 @@
-// TODO нужно сделать
+import FilterView from './view/filter-view.js';
+import {render} from './render.js';
+
+const siteMainElement = document.querySelector('.trip-controls');
+const siteHeaderElement = siteMainElement.querySelector('.trip-controls__filters');
+
+render(new FilterView(), siteHeaderElement);
