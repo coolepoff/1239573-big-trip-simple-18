b3dbd4a4b01b6738ebd45d0be9e70b61ab0ae7b6
coolepoff <97253599+coolepoff@users.noreply.github.com>
2022-08-11 11:57:10 +0300

    добавляет оставшиеся модули

--- src/main.js
+++ src/main.js
@@ -1,17 +1,30 @@
 import FiltersView from './view/filters-view.js';
 import SortView from './view/sort-view.js';
-import FormCreateView from './view/form-create-view.js';
+// import FormCreateView from './view/form-create-view.js';
 import FormEditView from './view/form-edit-fiew.js';
-import WaypointView from './view/waypoint.js';
+import WaypointListView from './view/waypoint-list.js';
+import WaypointItemView from './view/waypoint-item.js';
 import {render} from './render.js';
 
-const tripСontrolsElement = document.querySelector('.trip-controls');
+
+const tripPageHeaderElement = document.querySelector('.page-header');
+const tripСontrolsElement = tripPageHeaderElement.querySelector('.trip-controls');
 const filtersElement = tripСontrolsElement.querySelector('.trip-controls__filters');
-const tripEventsElement = document.querySelector('.trip-events');
+
+const tripPageMainElement = document.querySelector('.page-main');
+const tripEventsElement = tripPageMainElement.querySelector('.trip-events');
 
 render(new FiltersView(), filtersElement);
 render(new SortView(), tripEventsElement);
 
-render(new FormCreateView(), tripEventsElement);
-render(new FormEditView(), tripEventsElement);
-render(new WaypointView(), tripEventsElement);
+// render(new FormCreateView(), tripEventsElement);
+
+render(new WaypointListView(), tripEventsElement);
+
+const tripEventsListElement = tripPageMainElement.querySelector('.trip-events__list');
+
+render(new FormEditView(), tripEventsListElement);
+
+render(new WaypointItemView(), tripEventsListElement);
+render(new WaypointItemView(), tripEventsListElement);
+render(new WaypointItemView(), tripEventsListElement);
